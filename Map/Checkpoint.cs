965bc56d4156c6a9c534dbffb1d4ac2739a3261f
DuongAlern <142880959+Duongtunggiang@users.noreply.github.com>
2024-06-23 20:49:29 +0700

    Add files via upload

---
+++ Map/Checkpoint.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour{
+
+    private GameMaster gm;
+    private bool activated = false;
+    public Animator animator;
+    [SerializeField] AudioSource shineSound;
+
+    private void Start(){
+        gm = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<GameMaster>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other){
+
+        //Đặt vị trí checkpoint của trò chơi sau khi người chơi va chạm với checkpoint
+        if (other.CompareTag("Player")) {
+            if (!activated) {
+                shineSound.Play();
+                activated = true;
+                animator.SetTrigger("activate");
+                gm.lastCheckpointPos = transform.position;
+            }
+        }
+    }
+
+    //hủy checkpoint khi va chạm vào checkpoint
+    public void destroy() {
+        Destroy(gameObject);
+    }
+}
---
+++ Map/MovePlatform.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour
+{
+
+    //points for platform to travel to
+    public Transform startPoint;
+    public Transform endPoint;
+    public Transform startPos;
+    Vector3 nextPos;
+
+    public float movementSpeed;
+    public bool isMoving = true;
+
+    void Start(){
+        nextPos = startPos.position; //Di chuyển về phía vị trí ban đầu
+    }
+
+    void Update(){
+
+        //Đặt vị trí mục tiêu thích hợp
+        if (transform.position == startPoint.position){
+            nextPos = endPoint.position;
+        }
+        else if (transform.position == endPoint.position) {
+            nextPos = startPoint.position;
+        }
+
+        //Di chuyển nền tảng về phía mục tiêu
+        if (isMoving) {
+            transform.position = Vector3.MoveTowards(transform.position, nextPos, movementSpeed * Time.deltaTime);
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        //Đặt người chơi là một colision để di chuyển chúng cùng với nền tảng
+        if (collision.gameObject.CompareTag("Player")) {
+            isMoving = true;
+            collision.collider.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Player")){
+            collision.collider.transform.SetParent(null);
+        }
+    }
+
+    //Trực quan đường dẫn nền tảng
+    private void OnDrawGizmosSelected(){
+        Gizmos.DrawLine(startPoint.position, endPoint.position);
+    }
+}
